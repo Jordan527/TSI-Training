f41a8abeec26429f67907f4ff924cab37a762473
Jordan <jalibhai5526@gmail.com>
2024-01-17 17:49:06 +0100

    Reorganize + add number of coats feature

--- paintCalculator.py
+++ paintCalculator.py
@@ -1,20 +1,37 @@
-def getWidth():
-    width = input("What is the width of your wall in cm: ").strip()
-    while not width.isnumeric():
-        width = input("Please enter a valid width: ")
-    print()
-    return int(width) / 100
+# Ensures the input is a valid number
+def getValidNumber(value, item):
+    while not value.isnumeric():
+        value = input(f"please enter a valid {item}: ")
+    return int(value)
 
-def getHeight():
-    height = input("What is the height of your wall in cm: ").strip()
-    while not height.isnumeric():
-        height = input("Please enter a valid height: ")
-    print()
-    return int(height) / 100
+# Get the size value of a given dimension
+def getSize(dimension, item):
+    size = input(f"What is the {dimension} of your {item} in meters? ").strip()
+    return getValidNumber(size, dimension)
 
-width = getWidth()
-height = getHeight()
+# Get the number of coats wanted per user
+def getCoats():
+    options = (1, 2, 3)
+    
+    coats = input("How many coats of paint do you want? ").strip()
+    if coats.isnumeric():
+        coats = int(coats)
+    
+    while coats not in options:
+        coats = input(f"please enter a valid number between 1 and 3: ")
+        if coats.isnumeric():
+            coats = int(coats)
+
+    return coats
+
+width = getSize("width", "wall")
+height = getSize("height", "wall")
+
 area = width * height
+litresByArea = round(area / 10, 2) # 10m^2 per litre based on B&Q paint calculator
 
-litres = round(area / 10, 2) # 10m^2 per litre based on B&Q paint calculator
-print(litres)
+coats = getCoats()
+litresByCoats = litresByArea * coats
+print(litresByCoats)
+
+# TODO: Door is 1.5m^2, double door is 3m^2
--- tasks.py
+++ tasks.py
@@ -15,77 +15,84 @@
 # Task 4
 
 # Task 4.1
-x = 50
-y = 50
-if x > y:
-    print("Greater")
+# x = 50
+# y = 50
+# if x > y:
+#     print("Greater")
 
 # Task 4.2
-x = 50
-y = 50
-if x == y:
-    print("Equal")
+# x = 50
+# y = 50
+# if x == y:
+#     print("Equal")
     
 # Task 4.3
-def equalFunction(x, y):
-    if x == y:
-        print("Equal")
-    else:
-        print("Unequal")
+# def equalFunction(x, y):
+#     if x == y:
+#         print("Equal")
+#     else:
+#         print("Unequal")
 
-equalFunction(50, 50)
-equalFunction(51, 49)
+# equalFunction(50, 50)
+# equalFunction(51, 49)
 
 # Task 4.4
-def operationFunction(x, y):
-    if x == y:
-        print(1)
-    elif x > y:
-        print(2)
-    else:
-        print(3)
+# def operationFunction(x, y):
+#     if x == y:
+#         print(1)
+#     elif x > y:
+#         print(2)
+#     else:
+#         print(3)
         
-operationFunction(50, 50)
-operationFunction(57, 50)
-operationFunction(5, 50)
+# operationFunction(50, 50)
+# operationFunction(57, 50)
+# operationFunction(5, 50)
 
 # Task 5
-def matchFunction(day):
-    match day:
-        case 1:
-            print("Saturday")
-        case 2:
-            print("Sunday")
-        case _:
-            print("Weekday")
+# def matchFunction(day):
+#     match day:
+#         case 1:
+#             print("Saturday")
+#         case 2:
+#             print("Sunday")
+#         case _:
+#             print("Weekday")
 
-matchFunction(2)
-matchFunction(4)
+# matchFunction(2)
+# matchFunction(4)
 
 
 # Task 6.1
-i = 0
-while i < 6:
-    print(i)
-    i += 1
+# i = 0
+# while i < 6:
+#     print(i)
+#     i += 1
     
 # Task 6.2
-for i in range(6):
-    print(i)
-    if i == 3:
-        break
+# for i in range(6):
+#     print(i)
+#     if i == 3:
+#         break
 
 # Task 6.3
-i = 3
-while i <= 8:
-    if not i == 6:
-        print(i)
-    i += 1
+# i = 3
+# while i <= 8:
+#     if not i == 6:
+#         print(i)
+#     i += 1
 
 # Task 6.4
-list1 = [1, 2, 3, 4, 5]
-list2 = ["apple", "banana", "cherry"]
+# list1 = [1, 2, 3, 4, 5]
+# list2 = ["apple", "banana", "cherry"]
 
-for num in list1:
-    for fruit in list2:
-        print(f"{num}, {fruit}")
+# for num in list1:
+#     for fruit in list2:
+#         print(f"{num}, {fruit}")
+        
+# Task 7
+def getTotal(num1, num2):
+    return num1 + num2
+
+print(getTotal(3, 5))
+    
